0927b2c05435404e8d18159fec8975c6ef852a2f
omipapal12 <omipapal12@gmail.com>
2021-07-12 11:48:00 +0530

    added:employe wage

---
+++ src/EmpWageBuilder.java
@@ -0,0 +1,18 @@
+
+
+public class EmpWageBuilder {
+				public static void main(String[] args) {
+					System.out.println("Welcome");
+					// Constants
+					int IS_FULL_TIME = 1;
+					//Computation
+					double empCheck = Math.floor(Math.random() * 10) %2;
+					if (empCheck == IS_FULL_TIME) {
+						System.out.println("Employee is present");
+					}
+					else {
+						System.out.println("Employee is absent");
+					}
+				}
+}
+
