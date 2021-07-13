c4ea6b608d5fa1364f7b65e1a29828d85b24d309
omipapal12 <omipapal12@gmail.com>
2021-07-13 18:11:17 +0530

    Added: Daily Emp Wage

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -5,14 +5,19 @@ public class EmpWageBuilder {
 					System.out.println("Welcome");
 					// Constants
 					int IS_FULL_TIME = 1;
+					int EMP_RATE_PER_HOUR = 20;
+					// Variables
+					int empHrs = 0;
+					int empWage = 0;
 					//Computation
 					double empCheck = Math.floor(Math.random() * 10) %2;
-					if (empCheck == IS_FULL_TIME) {
-						System.out.println("Employee is present");
-					}
-					else {
-						System.out.println("Employee is absent");
+					if (empCheck == IS_FULL_TIME) 
+						empHrs = 8;
+					else
+						empHrs = 0;
+					empWage = empHrs * EMP_RATE_PER_HOUR;
+						System.out.println("Emp Wage:" + empWage);
 					}
 				}
-}
+
 
