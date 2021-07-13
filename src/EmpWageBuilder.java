87553adec265e0ee88b806bbd4491e3c43c73805
omipapal12 <omipapal12@gmail.com>
2021-07-13 18:31:09 +0530

    Added: Part Time Wage

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -4,14 +4,17 @@ public class EmpWageBuilder {
 				public static void main(String[] args) {
 					System.out.println("Welcome");
 					// Constants
-					int IS_FULL_TIME = 1;
+					int IS_PART_TIME = 1;
+					int IS_FULL_TIME = 2;
 					int EMP_RATE_PER_HOUR = 20;
 					// Variables
 					int empHrs = 0;
 					int empWage = 0;
 					//Computation
-					double empCheck = Math.floor(Math.random() * 10) %2;
-					if (empCheck == IS_FULL_TIME) 
+					double empCheck = Math.floor(Math.random() * 10) %3;
+					if (empCheck == IS_PART_TIME)
+						empHrs = 4;
+					else if (empCheck == IS_FULL_TIME) 
 						empHrs = 8;
 					else
 						empHrs = 0;
